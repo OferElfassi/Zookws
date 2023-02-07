3c1e84fb59b1a2cedd480467f83492a5982dc292
Ofer Elfassi <ofer221@hotmail.com>
2023-02-07 03:55:38 +0200

    initial template

---
+++ zoobar/auth_client.py
@@ -0,0 +1,18 @@
+from debug import *
+from zoodb import *
+import rpclib
+
+
+def login(username, password):
+    pass
+    # Fill in code here.
+
+
+def register(username, password):
+    pass
+    # Fill in code here.
+
+
+def check_token(username, token):
+    pass
+    # Fill in code here.
