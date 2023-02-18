0c94120711411848785499dc4aeec473b6c8d2d8
Ofer Elfassi <ofer221@hotmail.com>
2023-02-18 04:55:26 +0200

    adjust login to support the new auth_client for authentication

--- zoobar/login.py
+++ zoobar/login.py
@@ -6,14 +6,14 @@ from zoodb import *
 import auth
 import bank
 import random
-
+import auth_client
 
 class User(object):
     def __init__(self):
         self.person = None
 
     def checkLogin(self, username, password):
-        token = auth.login(username, password)
+        token = auth_client.login(username, password)
         if token is not None:
             return self.loginCookie(username, token)
         else:
@@ -27,17 +27,17 @@ class User(object):
         self.person = None
 
     def addRegistration(self, username, password):
-        token = auth.register(username, password)
+        token = auth_client.register(username, password)
         if token is not None:
             return self.loginCookie(username, token)
         else:
             return None
 
     def checkCookie(self, cookie):
-        if cookie is None:
+        if not cookie:
             return
         (username, token) = cookie.rsplit("#", 1)
-        if auth.check_token(username, token):
+        if auth_client.check_token(username, token):
             self.setPerson(username, token)
 
     def setPerson(self, username, token):
@@ -45,7 +45,6 @@ class User(object):
         self.person = persondb.query(Person).get(username)
         self.token = token
         self.zoobars = bank.balance(username)
-
 
 def logged_in():
     g.user = User()
@@ -55,7 +54,6 @@ def logged_in():
     else:
         return False
 
-
 def requirelogin(page):
     @wraps(page)
     def loginhelper(*args, **kwargs):
@@ -63,9 +61,7 @@ def requirelogin(page):
             return redirect(url_for('login') + "?nexturl=" + request.url)
         else:
             return page(*args, **kwargs)
-
     return loginhelper
-
 
 @catch_err
 def login():
@@ -109,7 +105,6 @@ def login():
                            nexturl=nexturl,
                            login_error=login_error,
                            login_username=Markup(request.form.get('login_username', '')))
-
 
 @catch_err
 def logout():
