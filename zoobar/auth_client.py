3c8bb7e05e9793a64673d0931f968717f1c9fd55
Ofer Elfassi <ofer221@hotmail.com>
2023-02-18 04:50:59 +0200

    implement auth server and auth client

--- zoobar/auth_client.py
+++ zoobar/auth_client.py
@@ -4,15 +4,21 @@ import rpclib
 
 
 def login(username, password):
-    pass
-    # Fill in code here.
+    with rpclib.client_connect('/authsvc/sock') as c:
+        kwargs = {"username": username, "password": password}
+        ret = c.call('login', **kwargs)
+        return ret
 
 
 def register(username, password):
-    pass
-    # Fill in code here.
+    with rpclib.client_connect('/authsvc/sock') as c:
+        kwargs = {"username": username, "password": password}
+        ret = c.call('register', **kwargs)
+        return ret
 
 
 def check_token(username, token):
-    pass
-    # Fill in code here.
+    with rpclib.client_connect('/authsvc/sock') as c:
+        kwargs = {"username": username, "token": token}
+        ret = c.call('check_token', **kwargs)
+        return ret
