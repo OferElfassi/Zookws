d6953a9942b73f319117494a339322a72bb550a6
Ofer Elfassi <ofer221@hotmail.com>
2023-02-18 04:39:09 +0200

    split the person db

--- zoobar/zoodb.py
+++ zoobar/zoodb.py
@@ -1,19 +1,28 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
-# from sqlalchemy.ext.declarative import *
+from sqlalchemy.ext.declarative import *
 import os
+import sys
 from debug import *
 
 PersonBase = declarative_base()
 TransferBase = declarative_base()
+CredBase = declarative_base()
+
 
 class Person(PersonBase):
     __tablename__ = "person"
     username = Column(String(128), primary_key=True)
-    password = Column(String(128))
-    token = Column(String(128))
     zoobars = Column(Integer, nullable=False, default=10)
     profile = Column(String(5000), nullable=False, default="")
+
+
+class Cred(CredBase):
+    __tablename__ = "cred"
+    username = Column(String(128), primary_key=True)
+    password = Column(String(128))
+    token = Column(String(128))
+
 
 class Transfer(TransferBase):
     __tablename__ = "transfer"
@@ -23,26 +32,35 @@ class Transfer(TransferBase):
     amount = Column(Integer)
     time = Column(String)
 
+
 def dbsetup(name, base):
     thisdir = os.path.dirname(os.path.abspath(__file__))
-    dbdir   = os.path.join(thisdir, "db", name)
+    dbdir = os.path.join(thisdir, "db", name)
     if not os.path.exists(dbdir):
         os.makedirs(dbdir)
 
-    dbfile  = os.path.join(dbdir, "%s.db" % name)
-    engine  = create_engine('sqlite:///%s' % dbfile,
-                            isolation_level='SERIALIZABLE')
+    dbfile = os.path.join(dbdir, "%s.db" % name)
+    engine = create_engine('sqlite:///%s' % dbfile,
+                           isolation_level='SERIALIZABLE')
     base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
     return session()
 
+
 def person_setup():
     return dbsetup("person", PersonBase)
+
 
 def transfer_setup():
     return dbsetup("transfer", TransferBase)
 
-import sys
+
+def cred_setup():
+    return dbsetup("cred", CredBase)
+
+
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: %s [init-person|init-transfer]" % sys.argv[0])
@@ -53,5 +71,7 @@ if __name__ == "__main__":
         person_setup()
     elif cmd == 'init-transfer':
         transfer_setup()
+    elif cmd == 'init-cred':
+        cred_setup()
     else:
         raise Exception("unknown command %s" % cmd)
