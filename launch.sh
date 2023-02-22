5789f1b4c034424db0793984ee45c6f997afb652
Ofer Elfassi <ofer221@hotmail.com>
2023-02-22 02:18:00 +0200

    fix setup process

--- launch.sh
+++ launch.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
-#if [ "$UID" -eq 0 ]; then
-#    echo "Updating jail files"
-#    /bin/sh -c "./setup.sh; echo \$?"
-#fi
-pgrep process_name1 process_name2 process_name3 | xargs kill
-current_dir=$(pwd)
-echo "Activating virtual environment"
-source /var/okws/run/venv/bin/activate
-echo "Running zookld in a new terminal"
-x-terminal-emulator -e "bash -c 'cd $current_dir/; sudo ./zookld 8080 $@; read'"
-kill $(pgrep zookld) $(pgrep zookd) $(pgrep zookhttp)
+
+PROJECT_DIR="/home/kali/CLionProjects/Zookws"
+
+port=${1:-8080}
+full_setup=${2:-0}
+
+# assert root
+if id | grep -qv uid=0; then
+  echo "Must be root"
+  exit 1
+fi
+
+# kill zookld if running
+echo "Killing zookld"
+sudo kill -9 $(pgrep zookld)
+wait $!
+
+
+if [ $full_setup = 1 ]; then
+  # Clean up
+  echo "Cleaning up"
+  make clean -C "$PROJECT_DIR"
+  wait $!
+  # Build
+  echo "Building"
+  make -C "$PROJECT_DIR"
+  wait $!
+  # setup JAIL
+  echo "Setting up JAIL"
+  "$PROJECT_DIR"/setup.sh
+  wait $!
+fi
+
+# launch
+echo "Launching on port $port"
+"$PROJECT_DIR"/zookld $port
+
